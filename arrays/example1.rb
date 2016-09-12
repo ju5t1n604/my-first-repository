9321cff150362a70f9e88a9615e32f9ab26a0de8
Justin Gray <justin.michael.gray@gmail.com>
2016-09-12 16:49:55 -0400

    Saved loops_iterators/exercise2.rb. Completed arrays examples.

---
+++ arrays/example1.rb
@@ -0,0 +1,7 @@
+# example1.rb
+
+arr = [1,3,5,7,9,11]
+
+if arr.include?(3)
+  puts "Yes, the value 3 is included in the array"
+end
---
+++ arrays/example7.rb
@@ -0,0 +1,11 @@
+# example7.rb
+
+arr1 = [1,2,3,4]
+arr2 = []
+
+arr1.each do |i|
+  arr2 << i + 2
+end
+
+p arr1
+p arr2
